555a83cf3542e51937561e5bfb65d4f227155c32
Guilherme Sampaio <guicsmpaio@gmail.com>
2025-06-07 14:47:57 -0300

    feat: integrate AngularFire for Firebase authentication
    - Added @angular/fire dependency to package.json.
    - Updated app.module.ts to include AngularFireModule and AngularFireAuthModule for Firebase integration.
    - Created environment.ts file to store Firebase configuration settings.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+import { environment } from '../environments/environment';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
+  declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
   ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCHo3g1JfYEUB-bT4x1v-0gRqICQ5uoyug',
+    authDomain: 'asimovies-7b35d.firebaseapp.com',
+    projectId: 'asimovies-7b35d',
+    storageBucket: 'asimovies-7b35d.firebasestorage.app',
+    messagingSenderId: '28053951943',
+    appId: '1:28053951943:web:56c0ce1cf396659b0c35e2',
+    measurementId: 'G-SV9F5KCB9M',
+  },
+};
